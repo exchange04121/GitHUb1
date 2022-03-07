dfd7ef4802d924041267cdb0b0748e0fe7340d7d
osoka <osman.bozdag@yahoo.com>
2022-03-07 22:11:02 +0100

    yeni

--- Github1/src/Main.java
+++ Github1/src/Main.java
@@ -3,5 +3,6 @@ public class Main
     public static void main(String[] args) {
         System.out.println("ilk");
         System.out.println("second");
+        System.out.println("third");
     }
 }
